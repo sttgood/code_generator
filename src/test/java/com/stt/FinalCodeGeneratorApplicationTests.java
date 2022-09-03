7bf348ba516f79ce7dafc1b792c89b5e2f67a260
sttgood <776599148@qq.com>
2022-09-03 12:08:05 +0800

    master change

--- src/test/java/com/stt/FinalCodeGeneratorApplicationTests.java
+++ src/test/java/com/stt/FinalCodeGeneratorApplicationTests.java
@@ -12,6 +12,7 @@ class FinalCodeGeneratorApplicationTests {
 		System.out.println("this is next version");
 		System.out.println("this is next next version");
 		System.out.println("this is hot-fix2 version");
+		System.out.println("this is master change!");
 	}
 
 }
